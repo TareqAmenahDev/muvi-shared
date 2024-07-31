56ba2b640de252b8a6bbe229624a0a4cc789aa2a
Tareq Amenah <t.amenah@alpha-apps.ae>
2024-07-31 11:30:40 +0300

    add gradle task to copy publish cocoa XCFramework

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -10,6 +10,9 @@ repositories {
     mavenLocal()
 }
 
+val versionName = "0.0.1"
+val iosDeploymentTarget = "16.0"
+
 kotlin {
     androidTarget {
         compilations.all {
@@ -25,8 +28,8 @@ kotlin {
     cocoapods {
         summary = "Some description for the Shared Module"
         homepage = "Link to the Shared Module homepage"
-        version = "0.0.1"
-        ios.deploymentTarget = "16.0"
+        version = versionName
+        ios.deploymentTarget = iosDeploymentTarget
         framework {
             baseName = "MuviShared"
             isStatic = true
@@ -46,26 +49,33 @@ kotlin {
         publications {
             create<MavenPublication>("muvishared") {
                 from(components["kotlin"])
-
                 groupId = "com.muvi"
                 artifactId = "muvishared"
-                version = "0.1.0"
+                version = versionName
             }
         }
 
-        repositories {
-            maven {
-                url = uri("https://maven.pkg.github.com/your-username/your-repo")
-                credentials {
-                    username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
-                    password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
-                }
-            }
-        }
+//        repositories {
+//            maven {
+//                url = uri("https://maven.pkg.github.com/your-username/your-repo")
+//                credentials {
+//                    username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+//                    password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+//                }
+//            }
+//        }
     }
 }
 
 task("testClasses")
+
+tasks.register<Copy>("publishMuviSharedXCFramework") {
+    dependsOn("podPublishXCFramework") // Ensure the build task runs before copyFiles
+
+    from("build/cocoapods/publish/release") // Source directory
+    into("../muvi-shared-cocoa") // Destination directory inside the project
+
+}
 
 android {
     namespace = "com.muvi.muvishared"
